b0aa46b6012fcfc30135ee6a5d1724784396f9c5
Suman75420 <152959601+Suman75420@users.noreply.github.com>
2025-07-01 13:14:54 +0530

    models

--- public/src/utils/asyncHandler.js
+++ public/src/utils/asyncHandler.js
@@ -1,6 +1,7 @@
-const asyncHandler = () => {
-
-(req,res,next) => {
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+       Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+   
 
 }
 }
