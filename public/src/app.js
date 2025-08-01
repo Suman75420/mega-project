adddb9441648c0ae1537173df6f53f04a45d94f2
Suman75420 <152959601+Suman75420@users.noreply.github.com>
2025-08-01 13:11:29 +0530

    all setup configured

--- public/src/app.js
+++ public/src/app.js
@@ -15,6 +15,11 @@ app.use(express.urlencoded({extented : true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import 
+import userRouter from './routes/user.routes.js'
 
+//routes declaration
+app.use("/api/v1/users",userRouter)
 
+//http://localhost:8000/api/v1/users/register
 export { app }
